2f4a4c764f6eae8f1968dc2dd1f4cf0d5f880b15
Dominik Orzechowski <domin852321@gmail.com>
2020-03-14 16:11:15 +0100

    14.03.2020 2

---
+++ ViewModels/CityVM.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstSide.ViewModels
+{
+    public enum CityVM
+    {
+            Wrocław,
+            Toruń,
+            Lublin,
+            Łódz,
+            Kraków,
+            Warszawa,
+            Opole,
+            Rzeszów,
+            Białystok,
+            Gdańsk,
+            Katowice,
+            Kielce,
+            Olsztyn,
+            Poznań
+    }
+}
